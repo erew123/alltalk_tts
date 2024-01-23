b3f53e93cc884315d0a147020c29048ef7cadc18
Josh XT <josh@devxt.com>
2024-01-22 20:46:53 -0500

    fix typo

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python modeldownload.py
-uvicorn tts_server:app --host 0.0.0.0 --port 7581 --workers 1 --proxy-headers &
+uvicorn tts_server:app --host 0.0.0.0 --port 7851 --workers 1 --proxy-headers &
 sleep 5
 python script.py
