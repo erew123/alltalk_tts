3de99331567f1008ad5a5a1953f6b37af48257e4
erew123 <35898566+erew123@users.noreply.github.com>
2024-03-27 20:42:32 +0000

    Add files via upload

--- atsetup.sh
+++ atsetup.sh
@@ -64,11 +64,9 @@ webui_menu() {
         echo -e "    in the ${L_GREEN}text-generation-webui${NC} folder and then re-run this script."
         echo
         echo "    BASE REQUIREMENTS"
-        echo -e "    1) Install the AllTalk requirements for an ${L_GREEN}Nvidia GPU machine.${NC}"
-        echo -e "    2) Install the AllTalk requirements for an ${L_GREEN}AMD or MAC machine.${NC}"
+        echo -e "    1) Install the AllTalk requirements for an ${L_GREEN}Text-generation-webui${NC}"
         echo
         echo "    OPTIONAL"
-        echo "    3) Install the Finetuning requirements."
         echo "    4) Install DeepSpeed."
         echo "    5) Uninstall DeepSpeed."
         echo
@@ -81,8 +79,6 @@ webui_menu() {
 
         case $webui_option in
             1) install_nvidia_textgen ;;
-            2) install_other_textgen ;;
-            3) install_finetune_textgen ;;
             4) install_deepspeed ;;
             5) uninstall_deepspeed ;;
             6) generate_diagnostics_textgen ;;
@@ -103,43 +99,6 @@ install_nvidia_textgen() {
         return
     fi
     echo "    Nvidia requirements installed successfully."
-    read -p "    Press any key to continue. " -n 1
-    echo
-}
-
-install_other_textgen() {
-    local requirements_file="requirements_other.txt"
-    echo "    Installing Other machine requirements from $requirements_file..."
-    if ! pip install -r "$requirements_file"; then
-        echo "    There was an error installing the Other requirements."
-        echo "    Please check the output for details."
-        read -p "    Press any key to return to the menu. " -n 1
-        echo
-        return
-    fi
-    echo "    Other machine requirements installed successfully."
-    read -p "    Press any key to continue. " -n 1
-    echo
-}
-
-install_finetune_textgen() {
-    local requirements_file="requirements_finetune.txt"
-    echo "    Installing Finetune requirements from $requirements_file..."
-    if ! pip install -r "$requirements_file"; then
-        echo "    There was an error installing the Finetune requirements."
-        echo "    Please check the output for details."
-        read -p "    Press any key to return to the menu. " -n 1
-        echo
-        return
-    fi
-    echo
-    echo "    Finetune base requirements installed successfully."
-    echo
-    echo -e "    Finetuning requires access to ${GREEN}Cublas${NC} from the ${GREEN}Nvidia CUDA Toolkit${NC}, so will need installing."
-    echo
-    echo -e "    Instructions can be found here:"
-    echo -e "    ${GREEN}https://github.com/erew123/alltalk_tts?#-finetuning-a-model${NC}"
-    echo
     read -p "    Press any key to continue. " -n 1
     echo
 }
@@ -221,7 +180,6 @@ standalone_menu() {
         echo
         echo "    OPTIONAL"
         echo "    2) Delete AllTalk's custom Python environment"
-        echo "    3) Install the Finetuning requirements"
         echo "    4) Install DeepSpeed."
         echo
         echo "    OTHER"        
@@ -234,7 +192,6 @@ standalone_menu() {
         case $standalone_option in
             1) install_custom_standalone ;;
             2) delete_custom_standalone ;;
-            3) install_finetune_standalone ;;
             4) install_deepspeed ;;
             5) generate_diagnostics_standalone ;;
             9) exit 0 ;;
@@ -284,39 +241,15 @@ install_custom_standalone() {
     # Activate the environment and install requirements
     source "${CONDA_ROOT_PREFIX}/etc/profile.d/conda.sh"
     conda activate "${INSTALL_ENV_DIR}"
-
     echo
-    echo -e "    ${L_BLUE}Choose the type of requirements to install${NC}"
+    echo "    Downloading and installing PyTorch. This step can take a long time"
+    echo "    depending on your internet connection and hard drive speed. Please"
+    echo "    be patient."
+    pip install torch>=2.2.1+cu121 torchaudio>=2.2.1+cu121 --extra-index-url https://download.pytorch.org/whl/cu121
     echo
-    echo "    1. Nvidia graphics card machines"
-    echo "    2. Other machines (mac, amd, etc)"
+    echo "    Installing additional requirements."
     echo
-    read -p "    Enter your choice (1 or 2): " user_choice
-
-    if [ "$user_choice" == "1" ]; then
-    	pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --upgrade --force-reinstall --index-url https://download.pytorch.org/whl/cu121
-    	pip install numpy==1.24.4
-	pip install typing-extensions>=4.8.0
-	pip install networkx<3.0.0,>=2.5.0
-    	pip install fsspec>=2023.6.0
-	pip install soundfile==0.12.1
-    	pip install uvicorn==0.24.0.post1
-        pip install transformers>=4.37.1
-    	pip install TTS==0.22.0
-    	pip install fastapi==0.104.1
-    	pip install Jinja2==3.1.2
-    	pip install requests==2.31.0
-    	pip install tqdm==4.66.1
-    	pip install importlib-metadata==4.8.1
-    	pip install packaging==23.2
-    	pip install pydantic==1.10.13
-    	pip install sounddevice==0.4.6
-    	pip install python-multipart==0.0.6
-        pip install cutlet>=0.3.0
-    	pip install unidic-lite>=1.0.8
-    else
-        pip install -r requirements_other.txt
-    fi
+    pip install -r system/requirements/requirements_standalone.txt
     # Create start_environment.sh to run AllTalk
     cat << EOF > start_environment.sh
 #!/bin/bash
@@ -343,16 +276,25 @@ source "${CONDA_ROOT_PREFIX}/etc/profile.d/conda.sh"
 conda activate "${INSTALL_ENV_DIR}"
 python script.py
 EOF
+    # Create start_finetune.sh to run AllTalk
+    cat << EOF > start_finetune.sh
+#!/bin/bash
+export TRAINER_TELEMETRY=0
+source "${CONDA_ROOT_PREFIX}/etc/profile.d/conda.sh"
+conda activate "${INSTALL_ENV_DIR}"
+python finetune.py
+EOF
     chmod +x start_alltalk.sh
     chmod +x start_environment.sh
+    chmod +x start_finetune.sh
     echo
     echo
-    echo -e "    start_alltalk.sh has been created."
-    echo -e "    You can now start AllTalk with ${L_YELLOW}./start_alltalk.sh${NC}"
+    echo -e "    Run ${L_YELLOW}./start_alltalk.sh${NC} to start AllTalk."
+    echo -e "    Run ${L_YELLOW}./start_finetune.sh${NC} to start Finetuning."
+    echo -e "    Run ${L_YELLOW}./start_environment.sh${NC} to start the AllTalk Python environment."
     echo
     read -p "    Press any key to continue. " -n 1
 }
-
 
 delete_custom_standalone() {
     local env_dir="$PWD/alltalk_environment"
@@ -376,40 +318,6 @@ delete_custom_standalone() {
     else
         echo "    Environment \"$env_dir\" deleted successfully."
     fi
-    read -p "    Press any key to continue. " -n 1
-    echo
-}
-
-install_finetune_standalone() {
-    local env_dir="$PWD/alltalk_environment"
-    local conda_root_prefix="${env_dir}/conda"
-    local install_env_dir="${env_dir}/env"
-    if [ ! -d "${install_env_dir}" ]; then
-        echo "    The Conda environment at '${install_env_dir}' does not exist."
-        echo "    Please install the environment before proceeding."
-        read -p "    Press any key to return to the menu. " -n 1
-        echo
-        return
-    fi
-    source "${conda_root_prefix}/etc/profile.d/conda.sh"
-    conda activate "${install_env_dir}"
-    local requirements_file="requirements_finetune.txt"
-    echo "Installing Finetune requirements from $requirements_file..."
-    if ! pip install -r "$requirements_file"; then
-        echo "    There was an error installing the Finetune requirements."
-        echo "    Please check the output for details."
-        read -p "    Press any key to return to the menu. " -n 1
-        echo
-        return
-    fi
-    echo
-    echo "    Finetune base requirements installed successfully."
-    echo
-    echo -e "    Finetuning requires access to ${GREEN}Cublas${NC} from the ${GREEN}Nvidia CUDA Toolkit${NC}, so will need installing."
-    echo
-    echo -e "    Instructions can be found here:"
-    echo -e "    ${GREEN}https://github.com/erew123/alltalk_tts?#-finetuning-a-model${NC}"
-    echo
     read -p "    Press any key to continue. " -n 1
     echo
 }
