012b987ab8d9274ba988fe6c3939696c373f5839
erew123 <35898566+erew123@users.noreply.github.com>
2024-06-14 13:10:17 +0100

    Force PyTorch 2.2.1

--- atsetup.sh
+++ atsetup.sh
@@ -325,7 +325,7 @@ install_custom_standalone() {
     echo "    Downloading and installing PyTorch. This step can take a long time"
     echo "    depending on your internet connection and hard drive speed. Please"
     echo "    be patient."
-    pip install torch>=2.2.1+cu121 torchaudio>=2.2.1+cu121 --upgrade --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu121
+    pip install torch==2.2.1+cu121 torchaudio==2.2.1+cu121 --upgrade --force-reinstall --extra-index-url https://download.pytorch.org/whl/cu121
     echo
     echo "    Installing additional requirements."
     echo
