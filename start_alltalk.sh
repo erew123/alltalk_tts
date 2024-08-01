7e2d3455d2234c6807afdf627124de8cfbf6ee2e
IIEleven11 <jakemottola6@gmail.com>
2024-08-01 00:05:42 -0700

    removed unecessary scripts and finetune.py edit

--- start_alltalk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source "/home/eleven/alltalkbeta/alltalk_tts/alltalk_environment/conda/etc/profile.d/conda.sh"
-conda activate "/home/eleven/alltalkbeta/alltalk_tts/alltalk_environment/env"
-python script.py
--- start_finetune.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export TRAINER_TELEMETRY=0
-source "/home/eleven/alltalkbeta/alltalk_tts/alltalk_environment/conda/etc/profile.d/conda.sh"
-conda activate "/home/eleven/alltalkbeta/alltalk_tts/alltalk_environment/env"
-python finetune.py
