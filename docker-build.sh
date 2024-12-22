79c26243d650c695ab5993be09c1463d7050b12a
erew123 <35898566+erew123@users.noreply.github.com>
2024-12-22 16:26:01 +0000

    Update docker-build.sh
    make build directories

--- docker-build.sh
+++ docker-build.sh
@@ -9,6 +9,11 @@ TTS_MODEL=xtts
 DOCKER_TAG=latest
 CLEAN=false
 
+# Create required build directories if they don't exist
+mkdir -p ${SCRIPT_DIR=}/docker/conda/build
+mkdir -p ${SCRIPT_DIR=}/docker/deepspeed/build
+echo "Created build directories"
+
 # Parse arguments
 while [ "$#" -gt 0 ]; do
   case "$1" in
@@ -42,6 +47,10 @@ done
 if [ "$CLEAN" = true ]; then
   rm -rf ${SCRIPT_DIR=}/docker/conda/build
   rm -rf ${SCRIPT_DIR=}/docker/deepspeed/build
+  # Recreate directories after clean
+  mkdir -p ${SCRIPT_DIR=}/docker/conda/build
+  mkdir -p ${SCRIPT_DIR=}/docker/deepspeed/build
+  echo "Cleaned and recreated build directories"  
 fi
 
 $SCRIPT_DIR/docker/conda/build-conda-env.sh \
