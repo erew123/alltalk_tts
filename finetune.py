990d9b2d9a128b10d95d8b60a667e8fe1bd6db75
j.katzwinkel@posteo.de <j.katzwinkel@posteo.de>
2025-02-26 21:53:57 +0100

    Revert error level changes

--- finetune.py
+++ finetune.py
@@ -273,21 +273,21 @@ def debug_print(
     elif is_info:
         prefix += ""
 
-    if level == "GPU_MEMORY" and (DebugLevels.GPU_MEMORY or is_error):
+    if level == "GPU_MEMORY" and DebugLevels.GPU_MEMORY:
         print(f"{prefix} [GPU] {message}")
-    elif level == "MODEL_OPS" and (DebugLevels.MODEL_OPS or is_error):
+    elif level == "MODEL_OPS" and DebugLevels.MODEL_OPS:
         print(f"{prefix} [MODEL] {message}")
-    elif level == "DATA_PROCESS" and (DebugLevels.DATA_PROCESS or is_error):
+    elif level == "DATA_PROCESS" and DebugLevels.DATA_PROCESS:
         print(f"{prefix} [DATA] {message}")
-    elif level == "GENERAL" and (DebugLevels.GENERAL or is_error):
+    elif level == "GENERAL" and DebugLevels.GENERAL:
         print(f"{prefix} [INFO] {message}")
-    elif level == "AUDIO" and (DebugLevels.AUDIO or is_error):
+    elif level == "AUDIO" and DebugLevels.AUDIO:
         print(f"{prefix} [AUDIO] {message}")
-    elif level == "SEGMENTS" and (DebugLevels.SEGMENTS or is_error):
+    elif level == "SEGMENTS" and DebugLevels.SEGMENTS:
         print(f"{prefix} [SEG] {message}")
-    elif level == "DUPLICATES" and (DebugLevels.DUPLICATES or is_error):
+    elif level == "DUPLICATES" and DebugLevels.DUPLICATES:
         print(f"{prefix} [DUP] {message}")
-    elif level == "VALIDATION" and (DebugLevels.VALIDATION or is_error):
+    elif level == "VALIDATION" and DebugLevels.VALIDATION:
         print(f"{prefix} [VAL] {message}")        
 
 
